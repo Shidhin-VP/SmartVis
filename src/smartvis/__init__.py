6d182442f5d9d159c79c0f6f57387fae2a5fa5ac
ShidhinVarghesePhilip <ShidhinVarghesePhilip98@gmail.com>
2025-08-03 19:34:02 -0500

    Phase 1 Finished Push

--- src/smartvis/__init__.py
+++ src/smartvis/__init__.py
@@ -2,13 +2,15 @@ import pandas as pd
 import plotly.express as px
 from itertools import combinations, permutations
 
-def visualizeEverything(df,maxPermutations=None,maxGraph=None,permute=False):
+def visualizeEverything(df,iColumns:list[str] | None=None,maxPermutations=None,maxGraph=None,permute=False):
     try:
         savedGraphMemory=maxGraph
         plottedPer=1
         plottedGraph=1
         dfNew=df.copy()
         dfNew.columns=dfNew.columns.str.strip()
+        if iColumns is not None:
+            dfNew=dfNew[list(map(str.strip,iColumns))]
         columns=dfNew.select_dtypes(include="object").columns
         for i in columns:
             if dfNew[i].nunique()<=20:
@@ -20,6 +22,7 @@ def visualizeEverything(df,maxPermutations=None,maxGraph=None,permute=False):
             return
         else:
             finColumns=list(permutations(finColumns,2) if permute==True else combinations(finColumns,2))
+            print(f"List of Permutation:\n{finColumns}" if permute else f"List of Combinations:\n{finColumns}")
             second=finColumns[0][1]
             for i, j in finColumns:
                 if (maxGraph is not None and maxPermutations is None and plottedGraph<=maxGraph) or (maxGraph is None and maxPermutations is not None and plottedPer<=maxPermutations):
